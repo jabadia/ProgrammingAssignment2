53d859be38891cb39213ebf15f7dff5dd32ff3c3
Javier Abadia <javier.abadia@esri.es>
2014-12-04 17:23:22 +0100

    comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## two functions that implement the programming asssignment 2 of r-programming course
 
-## Write a short comment describing this function
+## this function creates an object that contains the original matrix, and the inverse of it if it has been calculated
+## it also contains 4 methods that allow manipulating these values: 
+##		set/get for the matrix value 
+##		and setinverse,getinverse for the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 	inverse <- NULL
@@ -16,7 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## this function return the matrix inverse
+## x must have been created with makeCacheMatrix function from a matrix() object
+## if the inverse has already been calculated, it returns the cached value stored inside the object x
+## otherwise, it computes and stores the matrix inverse
 
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
